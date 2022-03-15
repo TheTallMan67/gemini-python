8a1cd52623c609ec18e25b8fd4ba86163963354a
Colin <mccormack.colin@gmail.com>
2022-03-14 22:18:45 -0400

    Revert "removed all websocket references"

--- gemini/__init__.py
+++ gemini/__init__.py
@@ -1,2 +1,6 @@
 from .public_client import PublicClient
 from .private_client import PrivateClient
+from .basewebsocket import BaseWebSocket
+from .marketdataws import MarketDataWS
+from .ordereventsws import OrderEventsWS
+from .order_book import GeminiOrderBook
